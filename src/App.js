7ab61c0061bccbb54c4ac02be38b412e149c4360
seongbin3931 <seongbin3931@gmail.com>
2021-11-15 17:16:09 +0900

    api test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import axios from "axios"
+import Food from './Food'
+import Button from './Button'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    data: [],
+    numOfRows: 5,
+    pageNo: 1,
+    pageSize: 10
+  }
+  
+  getFood() {
+    const API_KEY = process.env.REACT_APP_API_KEY
+    const url = `http://data.ex.co.kr/openapi/business/representFoodServiceArea?key=${API_KEY}&type=json&numOfRows=${this.state.numOfRows}&pageNo=${this.state.pageNo}`
+    
+    axios.get(url).then((res) => {
+      const data = res.data.list
+      const pageSize = res.data.pageSize
+      const pageNo = res.data.pageNo
+
+      console.log(data)
+      console.log(pageSize)
+      console.log(pageNo)
+
+      this.setState({
+        isLoading: false,
+        data: data,
+        pageSize: pageSize,
+      })
+    })
+  }
+
+  nextPage = () => {
+    this.setState({pageNo: this.state.pageNo<this.state.pageSize? this.state.pageNo+1:this.state.pageNo+0})
+    this.getFood()
+    console.log(this.state.pageNo)
+  }
+
+  prevPage = () => {
+    this.setState({pageNo: this.state.pageNo>0?this.state.pageNo-1:this.state.pageNo+0})
+    this.getFood()
+    console.log(this.state.pageNo)
+  }
+
+  componentDidMount() {
+    this.getFood()
+  }
+
+  componentDidUpdate() {
+    console.log('update')
+  }
+
+  render() {
+    const {data, pageNo} = this.state
+    const {nextPage, prevPage} = this
+
+    return(
+      <div>
+        
+        {data.map((d,cnt) => {
+          return (
+            <Food
+            key = {data[cnt].serviceAreaCode}
+            serviceAreaName = {data[cnt].serviceAreaName}
+            svarAddr = {data[cnt].svarAddr}
+            batchMenu = {data[cnt].batchMenu}
+            salePrice = {data[cnt].salePrice}
+            >
+          </Food>
+          )
+        })}
+        <Button handleClick={nextPage}>다음 페이지</Button>
+        <Button handleClick={prevPage}>이전 페이지</Button>
+        {pageNo}
+      </div>
+    )
+  }
 }
 
 export default App;
